e12b6f12dc1ac9356e17d2ed18c4c6d9fe28b8f6
Erik Rosenlund <erik.rosenlund@saabgroup.com>
2025-11-03 16:00:26 +0100

    Re added pch

---
+++ lib/include/pch.h
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <format>
+
+#include "cutecpp/log.hpp"
+
+#include "ssp4cpp/ssp.hpp"
+#include "ssp4cpp/fmu.hpp"
+
+#include "nlohmann/json.hpp"
