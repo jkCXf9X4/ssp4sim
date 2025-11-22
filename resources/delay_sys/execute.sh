c75815ecb1a95a6a05774360c8b152284d87e0d4
Erik Rosenlund <erik.rosenlund@saabgroup.com>
2025-11-22 14:12:49 +0100

    Some delay experimentation

--- resources/delay_sys/execute.sh
+++ resources/delay_sys/execute.sh
@@ -7,17 +7,18 @@
 # ./build/public/ssp4sim_app/sim_app ./resources/delay_sys/1_native_1ms.json
 # ./build/public/ssp4sim_app/sim_app ./resources/delay_sys/1_native_2ms.json
 # ./build/public/ssp4sim_app/sim_app ./resources/delay_sys/1_native_3ms.json
-# ./build/public/ssp4sim_app/sim_app ./resources/delay_sys/2_grouping_7ms.json
 # ./build/public/ssp4sim_app/sim_app ./resources/delay_sys/2_grouping_8ms.json
 
 
 # ./build/public/ssp4sim_app/sim_app ./resources/delay_sys/3_explicit_delay_1ms.json
 # ./build/public/ssp4sim_app/sim_app ./resources/delay_sys/3_explicit_delay_2ms.json
-# ./build/public/ssp4sim_app/sim_app ./resources/delay_sys/native_no_delay_1ms.json
 # ./build/public/ssp4sim_app/sim_app ./resources/delay_sys/native_no_delay_2ms.json
+# ./build/public/ssp4sim_app/sim_app ./resources/delay_sys/native_no_delay_1ms.json
 
-# cp ./resources/delay_sys/reference/reference_system_native.csv ./results/reference_results.csv
+./build/public/ssp4sim_app/sim_app ./resources/delay_sys/2_grouping_7ms.json
+./build/public/ssp4sim_app/sim_app ./resources/delay_sys/3_grouping.json
+cp ./resources/delay_sys/reference/reference_system_native.csv ./results/reference_results.csv
 
-python3 scripts/filter_results.py --window-size 3
+# python3 scripts/filter_results.py --window-size 3
 
 python3 ./resources/delay_sys/reference/compare.py
